131b376f5b9350d2e4b965cf8098ee2abfb0d081
Monica Crowe <mcrowe9194@gmail.com>
2022-05-25 22:35:07 -0500

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var celsius = TempConverter.FarhenheitToCelsius(68);
+            var farhenheit = TempConverter.CelciusToFarhenheite(20);
+
+            Console.WriteLine("After conversion");
+            Console.WriteLine($"celsius {celsius}");
+            Console.WriteLine($"farhenheit {farhenheit}");
+        }
+    }
+}
